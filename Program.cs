6976e5e4efb70e5b08b731d31dc6735adfdb8017
NintenHero <mchinrichs@gmail.com>
2024-05-22 10:14:30 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace FreeeInfantry_BLO_Extractor
+{
+    internal class Program
+    {
+        public static BinaryReader br;
+        static void Main(string[] args)
+        {
+            br = new(File.OpenRead(args[0]));
+            br.ReadInt32();
+            int fileCount = br.ReadInt32();
+
+            List<Subfile> subfiles = new();
+            for (int i = 0; i < fileCount; i++)
+            {
+                subfiles.Add(new());
+            }
+
+            string path = Path.GetDirectoryName(args[0]) + "\\" + Path.GetFileNameWithoutExtension(args[0]);
+            Directory.CreateDirectory(path);
+            foreach (Subfile file in subfiles)
+            {
+                br.BaseStream.Position = file.start;
+                BinaryWriter bw = new(File.Create(path + "\\" + file.name[0]));
+                bw.Write(br.ReadBytes(file.size));
+                bw.Close();
+            }
+        }
+
+        class Subfile
+        {
+            public string[] name = new string(br.ReadChars(32)).TrimEnd('\0').Split('\0');
+            public int start = br.ReadInt32();
+            public int size = br.ReadInt32();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FreeeInfantry-BLO-Extractor
+ A tool to extract from BLO files in the game [FreeInfantry](https://store.steampowered.com/app/2830720).
